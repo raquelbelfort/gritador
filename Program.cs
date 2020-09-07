71f142be43b776f4571cd13bd86ce5c0c63777b7
Raquel Belfort <raquelzinha.belfort@gmail.com>
2020-09-07 18:18:14 -0300

    Adição de resolução.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Olá, usuário. Digite qualquer coisa, e eu o direi gritando!");
+            string palavra = Console.ReadLine();
+            Console.WriteLine (palavra.ToUpper());
+        }
+    }
+}
